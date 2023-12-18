865823f64bd206251c454a3ace59551acb2fa17d
Alexandra Paramonova <alpar.wb@gmail.com>
2023-12-18 23:45:46 +0500

    fix: implement minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const resultArray = [];
+  for (let idxRow = 0; idxRow < matrix.length; idxRow += 1) {
+    resultArray[idxRow] = [];
+    for (let idxCol = 0; idxCol < matrix[0].length; idxCol += 1) {
+      const neighbors8 = [
+        matrix[idxRow - 1]?.[idxCol - 1],
+        matrix[idxRow - 1]?.[idxCol],
+        matrix[idxRow - 1]?.[idxCol + 1],
+        matrix[idxRow]?.[idxCol - 1],
+        matrix[idxRow]?.[idxCol + 1],
+        matrix[idxRow + 1]?.[idxCol - 1],
+        matrix[idxRow + 1]?.[idxCol],
+        matrix[idxRow + 1]?.[idxCol + 1],
+      ];
+      const definedNeighbors8 = neighbors8.filter((value) => typeof value !== 'undefined');
+      const neighborsMinesQty = definedNeighbors8.reduce((sum, value) => sum + +value, 0);
+      resultArray[idxRow][idxCol] = neighborsMinesQty;
+    }
+  }
+
+  return resultArray;
 }
 
 module.exports = {
