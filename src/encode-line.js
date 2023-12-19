197fac0ebb319825f19faf56bf08eab4e8fee190
Alexandra Paramonova <alpar.wb@gmail.com>
2023-12-19 16:48:57 +0500

    fix: implement encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (typeof str !== 'string') {
+    return undefined;
+  }
+
+  str = str.trim();
+  if (str.length === 0) {
+    return str;
+  }
+
+  const getLetterEncodePart = (letter, count) => {
+    const printNumber = count === 1 ? '' : count;
+    const letterEncodePart = `${printNumber}${letter}`;
+    return letterEncodePart;
+  }
+
+  let currentLetter = str[0];
+  let currentCount = 1;
+  let resultString = '';
+  for (let i = 1; i < str.length; i += 1) {
+    if (currentLetter === str[i]) {
+      currentCount += 1;
+    } else {
+      resultString += getLetterEncodePart(currentLetter, currentCount);
+      currentCount = 1;
+      currentLetter = str[i];
+    }
+  }
+  resultString += getLetterEncodePart(currentLetter, currentCount);
+
+  return resultString;
 }
 
 module.exports = {
